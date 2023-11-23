a6005ad698ff76af8f71d9b4a7818c54891f518c
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2023-11-23 10:45:33 +0100

    Primer commit

---
+++ src/application/Main.java
@@ -0,0 +1,42 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/view/PantallaPrincipal.fxml"));
+			
+			Pane ventana = (Pane) loader.load();
+
+			Scene scene = new Scene(ventana);
+			scene.getStylesheets().add(getClass().getResource("/view/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+			// Asignar icono
+			Image icon = new Image("resources/logo.png");
+			primaryStage.getIcons().add(icon);
+			
+			// Poner titulo a la ventana
+			primaryStage.setTitle("TU.PELI");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
