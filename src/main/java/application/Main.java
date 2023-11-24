0539fa1c11536be263bb809bd310450b67675a66
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2023-11-24 10:51:13 +0100

    Nuevo incio proyecto

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,45 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("/view/PantallaLogin.fxml"));
+			
+			Pane ventana = (Pane) loader.load();
+
+			Scene scene = new Scene(ventana);
+			scene.getStylesheets().add(getClass().getResource("/resources/css/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			
+			primaryStage.show();
+			
+			// Arranca maximizado
+			primaryStage.setMaximized(true);
+			
+			// Asignar icono
+			Image icon = new Image("resources/images/logo/logo.png");
+			primaryStage.getIcons().add(icon);
+			
+			// Poner titulo a la ventana
+			primaryStage.setTitle("TU.PELI");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
