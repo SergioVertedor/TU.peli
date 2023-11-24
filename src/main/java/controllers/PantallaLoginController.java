2e553376efbe67faa99dd3603a426c1f17e74893
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2023-11-24 12:14:41 +0100

    FX Login Action Buttons Ready

--- src/main/java/controllers/PantallaLoginController.java
+++ src/main/java/controllers/PantallaLoginController.java
@@ -1,14 +1,40 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class PantallaLoginController {
+
+    @FXML
+    private Button btnLoginEnter;
+
+    @FXML
+    private Button btnLoginRegister;
+
+    @FXML
+    private Button btnLoginWithGoogle;
 
     @FXML
     private TextField txtLoginPassword;
 
     @FXML
     private TextField txtLoginUser;
+
+    @FXML
+    void btnLoginEnterPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoginRegisterPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnLoginWithGooglePressed(ActionEvent event) {
+
+    }
 
 }
