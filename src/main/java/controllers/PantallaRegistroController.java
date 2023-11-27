9ed25dbe1a4451fab0eac0201fcb9afb2d18fbf2
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2023-11-27 11:52:10 +0100

    Controller Pantalla Registro

---
+++ src/main/java/controllers/PantallaRegistroController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class PantallaRegistroController {
+
+    @FXML
+    private Button btnLoginRegister;
+
+    @FXML
+    private ImageView imgLoginLogo;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtPasswordRepeat;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnRegisterNewUserPressed(ActionEvent event) {
+    	// TODO
+    }
+}
