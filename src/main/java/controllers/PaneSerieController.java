0ba24d292e6b586df62d6fd5619c589dcdcf2f91
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2024-01-16 11:14:57 +0100

    cambios

---
+++ src/main/java/controllers/PaneSerieController.java
@@ -0,0 +1,165 @@
+package controllers;
+
+import com.gluonhq.charm.glisten.control.Avatar;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class PaneSerieController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private HBox boxReparto1;
+
+    @FXML
+    private HBox boxReparto11;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    private Button btnTrailer;
+
+    @FXML
+    private HBox elementReparto1;
+
+    @FXML
+    private HBox hBoxRating;
+
+    @FXML
+    private ImageView imgFav1;
+
+    @FXML
+    private ImageView imgFav11;
+
+    @FXML
+    private ImageView imgFav12;
+
+    @FXML
+    private ImageView imgFav13;
+
+    @FXML
+    private ImageView imgFav14;
+
+    @FXML
+    private ImageView imgPoster;
+
+    @FXML
+    private Avatar imgReparto1;
+
+    @FXML
+    private Avatar imgReparto2;
+
+    @FXML
+    private Avatar imgReparto21;
+
+    @FXML
+    private Label lblDirectorGenero;
+
+    @FXML
+    private Label lblEstrenoTemporadas;
+
+    @FXML
+    private Label lblFechaVista;
+
+    @FXML
+    private Label lblReparto;
+
+    @FXML
+    private Label lblReparto1;
+
+    @FXML
+    private Label lblReparto2;
+
+    @FXML
+    private Label lblReparto21;
+
+    @FXML
+    private Label lblSinopsis;
+
+    @FXML
+    private Label lblTitulo;
+
+    @FXML
+    private Label lblTituloSinopsis;
+
+    @FXML
+    private Label lbluserRating;
+
+    @FXML
+    private VBox vBoxReparto;
+
+    @FXML
+    void fav1Selected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fav2Selected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fav3Selected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fav4Selected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void fav5Selected(MouseEvent event) {
+
+    }
+
+    @FXML
+    void userRatingPressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert boxReparto1 != null : "fx:id=\"boxReparto1\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert boxReparto11 != null : "fx:id=\"boxReparto11\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert btnAdd != null : "fx:id=\"btnAdd\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert btnTrailer != null : "fx:id=\"btnTrailer\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert elementReparto1 != null : "fx:id=\"elementReparto1\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert hBoxRating != null : "fx:id=\"hBoxRating\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgFav1 != null : "fx:id=\"imgFav1\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgFav11 != null : "fx:id=\"imgFav11\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgFav12 != null : "fx:id=\"imgFav12\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgFav13 != null : "fx:id=\"imgFav13\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgFav14 != null : "fx:id=\"imgFav14\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgPoster != null : "fx:id=\"imgPoster\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgReparto1 != null : "fx:id=\"imgReparto1\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgReparto2 != null : "fx:id=\"imgReparto2\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert imgReparto21 != null : "fx:id=\"imgReparto21\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblDirectorGenero != null : "fx:id=\"lblDirectorGenero\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblEstrenoTemporadas != null : "fx:id=\"lblEstrenoTemporadas\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblFechaVista != null : "fx:id=\"lblFechaVista\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblReparto != null : "fx:id=\"lblReparto\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblReparto1 != null : "fx:id=\"lblReparto1\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblReparto2 != null : "fx:id=\"lblReparto2\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblReparto21 != null : "fx:id=\"lblReparto21\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblSinopsis != null : "fx:id=\"lblSinopsis\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblTitulo != null : "fx:id=\"lblTitulo\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lblTituloSinopsis != null : "fx:id=\"lblTituloSinopsis\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert lbluserRating != null : "fx:id=\"lbluserRating\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+        assert vBoxReparto != null : "fx:id=\"vBoxReparto\" was not injected: check your FXML file 'PantallaSerie.fxml'.";
+
+    }
+
+}
