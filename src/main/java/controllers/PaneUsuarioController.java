f2bccc7ba719366bbc75109ce4e07db4ce47dac6
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2024-01-30 11:10:30 +0100

    PaneUsuario OK

---
+++ src/main/java/controllers/PaneUsuarioController.java
@@ -0,0 +1,136 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class PaneUsuarioController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnEditarDispositivos;
+
+    @FXML
+    private Button btnGuardar;
+
+    @FXML
+    private HBox hBoxDispositivo1;
+
+    @FXML
+    private HBox hBoxDispositivo1111;
+
+    @FXML
+    private HBox hBoxDispositivo2;
+
+    @FXML
+    private HBox hBoxDispositivo3;
+
+    @FXML
+    private ImageView imgDispositivo1;
+
+    @FXML
+    private ImageView imgDispositivo2;
+
+    @FXML
+    private ImageView imgDispositivo3;
+
+    @FXML
+    private ImageView imgDispositivo4;
+
+    @FXML
+    private ImageView imgUser;
+
+    @FXML
+    private Label lblDispositivo1;
+
+    @FXML
+    private Label lblDispositivo2;
+
+    @FXML
+    private Label lblDispositivo3;
+
+    @FXML
+    private Label lblDispositivo4;
+
+    @FXML
+    private Button lblExportar;
+
+    @FXML
+    private Button lblImportar;
+
+    @FXML
+    private Label lblMiembrodesde;
+
+    @FXML
+    private Label lblTNumTitulosLista;
+
+    @FXML
+    private Label lblUltimaConexion;
+
+    @FXML
+    private Label lblUsername;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private TextField txtPass;
+
+    @FXML
+    private TextField txtPass2;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void btnEditarDispositivosPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGuardarPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnEditarDispositivos != null : "fx:id=\"btnEditarDispositivos\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert btnGuardar != null : "fx:id=\"btnGuardar\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert hBoxDispositivo1 != null : "fx:id=\"hBoxDispositivo1\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert hBoxDispositivo1111 != null : "fx:id=\"hBoxDispositivo1111\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert hBoxDispositivo2 != null : "fx:id=\"hBoxDispositivo2\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert hBoxDispositivo3 != null : "fx:id=\"hBoxDispositivo3\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert imgDispositivo1 != null : "fx:id=\"imgDispositivo1\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert imgDispositivo2 != null : "fx:id=\"imgDispositivo2\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert imgDispositivo3 != null : "fx:id=\"imgDispositivo3\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert imgDispositivo4 != null : "fx:id=\"imgDispositivo4\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert imgUser != null : "fx:id=\"imgUser\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblDispositivo1 != null : "fx:id=\"lblDispositivo1\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblDispositivo2 != null : "fx:id=\"lblDispositivo2\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblDispositivo3 != null : "fx:id=\"lblDispositivo3\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblDispositivo4 != null : "fx:id=\"lblDispositivo4\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblExportar != null : "fx:id=\"lblExportar\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblImportar != null : "fx:id=\"lblImportar\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblMiembrodesde != null : "fx:id=\"lblMiembrodesde\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblTNumTitulosLista != null : "fx:id=\"lblTNumTitulosLista\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblUltimaConexion != null : "fx:id=\"lblUltimaConexion\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert lblUsername != null : "fx:id=\"lblUsername\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert txtEmail != null : "fx:id=\"txtEmail\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert txtPass != null : "fx:id=\"txtPass\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert txtPass2 != null : "fx:id=\"txtPass2\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+        assert txtUsername != null : "fx:id=\"txtUsername\" was not injected: check your FXML file 'PaneUsuario.fxml'.";
+
+    }
+
+}
