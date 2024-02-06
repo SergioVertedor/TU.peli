843cd1c3b84e0885c1b98ead2dab8e0cfa1cd6cc
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2024-02-06 12:16:43 +0100

    Pane MisListas OK

---
+++ src/main/java/controllers/PaneMisListasController.java
@@ -0,0 +1,278 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+public class PaneMisListasController {
+
+    @FXML
+    private Label directorPelicula00;
+
+    @FXML
+    private Label directorPelicula001;
+
+    @FXML
+    private Label directorPelicula002;
+
+    @FXML
+    private Label directorPelicula003;
+
+    @FXML
+    private Label directorPelicula004;
+
+    @FXML
+    private Label directorPelicula005;
+
+    @FXML
+    private Label directorPelicula006;
+
+    @FXML
+    private ImageView imgFav00;
+
+    @FXML
+    private ImageView imgFav01;
+
+    @FXML
+    private ImageView imgFav02;
+
+    @FXML
+    private ImageView imgFav03;
+
+    @FXML
+    private ImageView imgFav04;
+
+    @FXML
+    private ImageView imgFav05;
+
+    @FXML
+    private ImageView imgFav06;
+
+    @FXML
+    private ImageView imgPelicula00;
+
+    @FXML
+    private ImageView imgPelicula001;
+
+    @FXML
+    private ImageView imgPelicula002;
+
+    @FXML
+    private ImageView imgPelicula003;
+
+    @FXML
+    private ImageView imgPelicula004;
+
+    @FXML
+    private ImageView imgPelicula005;
+
+    @FXML
+    private ImageView imgPelicula006;
+
+    @FXML
+    private ImageView imgReciente00;
+
+    @FXML
+    private ImageView imgReciente01;
+
+    @FXML
+    private ImageView imgReciente02;
+
+    @FXML
+    private ImageView imgReciente03;
+
+    @FXML
+    private ImageView imgReciente04;
+
+    @FXML
+    private ImageView imgReciente05;
+
+    @FXML
+    private ImageView imgReciente06;
+
+    @FXML
+    private ImageView imgReciente07;
+
+    @FXML
+    private ImageView imgReciente08;
+
+    @FXML
+    private ImageView imgReciente09;
+
+    @FXML
+    private ImageView imgReciente10;
+
+    @FXML
+    private ImageView imgReciente11;
+
+    @FXML
+    private ImageView imgSerie00;
+
+    @FXML
+    private ImageView imgSerie01;
+
+    @FXML
+    private ImageView imgSerie02;
+
+    @FXML
+    private ImageView imgSerie03;
+
+    @FXML
+    private ImageView imgSerie04;
+
+    @FXML
+    private ImageView imgSerie05;
+
+    @FXML
+    private ImageView imgSerie06;
+
+    @FXML
+    private Label lblVerMasFavoritos;
+
+    @FXML
+    private Label lblVerMasPeliculas;
+
+    @FXML
+    private Label lblVerMasSeries;
+
+    @FXML
+    private HBox line0;
+
+    @FXML
+    private Label subtituloFav00;
+
+    @FXML
+    private Label subtituloFav01;
+
+    @FXML
+    private Label subtituloFav02;
+
+    @FXML
+    private Label subtituloFav03;
+
+    @FXML
+    private Label subtituloFav04;
+
+    @FXML
+    private Label subtituloFav05;
+
+    @FXML
+    private Label subtituloFav06;
+
+    @FXML
+    private Label temporadasSerie00;
+
+    @FXML
+    private Label temporadasSerie01;
+
+    @FXML
+    private Label temporadasSerie02;
+
+    @FXML
+    private Label temporadasSerie03;
+
+    @FXML
+    private Label temporadasSerie04;
+
+    @FXML
+    private Label temporadasSerie05;
+
+    @FXML
+    private Label temporadasSerie06;
+
+    @FXML
+    private Label tituloFav00;
+
+    @FXML
+    private Label tituloFav01;
+
+    @FXML
+    private Label tituloFav02;
+
+    @FXML
+    private Label tituloFav03;
+
+    @FXML
+    private Label tituloFav04;
+
+    @FXML
+    private Label tituloFav05;
+
+    @FXML
+    private Label tituloFav06;
+
+    @FXML
+    private Label tituloPelicula00;
+
+    @FXML
+    private Label tituloPelicula001;
+
+    @FXML
+    private Label tituloPelicula002;
+
+    @FXML
+    private Label tituloPelicula003;
+
+    @FXML
+    private Label tituloPelicula004;
+
+    @FXML
+    private Label tituloPelicula005;
+
+    @FXML
+    private Label tituloPelicula006;
+
+    @FXML
+    private Label tituloSerie00;
+
+    @FXML
+    private Label tituloSerie01;
+
+    @FXML
+    private Label tituloSerie02;
+
+    @FXML
+    private Label tituloSerie03;
+
+    @FXML
+    private Label tituloSerie04;
+
+    @FXML
+    private Label tituloSerie05;
+
+    @FXML
+    private Label tituloSerie06;
+
+    @FXML
+    void addMisPeliculasPressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void addMisSeriesPressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToTitulo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void verTodasMisPeliculas(MouseEvent event) {
+
+    }
+
+    @FXML
+    void verTodasMisSeries(MouseEvent event) {
+
+    }
+
+    @FXML
+    void verTodosMisFavoritos(MouseEvent event) {
+
+    }
+
+}
