1ad54d1773152d28257a57caff5a3aa914552c02
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2024-02-06 12:23:23 +0100

    Carga de imagenes en inicio

--- src/main/java/controllers/PantallaModeloController.java
+++ src/main/java/controllers/PantallaModeloController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
@@ -102,5 +103,7 @@ public class PantallaModeloController {
   @FXML
   void initialize() {
     PaneSwitcher.switchPane("PaneInicio", centralPane);
+    imgLogo.setImage(new Image("images/logo/logo.png"));
+    imgUser.setImage(new Image("images/user.png"));
   }
 }
