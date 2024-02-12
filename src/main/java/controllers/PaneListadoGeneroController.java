c1ab12c9cff213a66a3618ee8813716bd51f8322
EPPalom <a_eva.palomo.palomas@iespablopicasso.es>
2024-02-12 13:02:29 +0100

    Nuevo PaneListadoGenero para películas y series

---
+++ src/main/java/controllers/PaneListadoGeneroController.java
@@ -0,0 +1,76 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class PaneListadoGeneroController {
+
+	// Imagenes por fila 0-3 y columna 0-6
+  @FXML private ImageView imgResultado00;
+  @FXML private ImageView imgResultado01;
+  @FXML private ImageView imgResultado02;
+  @FXML private ImageView imgResultado03;
+  @FXML private ImageView imgResultado04;
+  @FXML private ImageView imgResultado05;
+  @FXML private ImageView imgResultado06;
+  
+  @FXML private ImageView imgResultado10;
+  @FXML private ImageView imgResultado11;
+  @FXML private ImageView imgResultado12;
+  @FXML private ImageView imgResultado13;
+  @FXML private ImageView imgResultado14;
+  @FXML private ImageView imgResultado15;
+  @FXML private ImageView imgResultado16;
+
+  @FXML private ImageView imgResultado20;
+  @FXML private ImageView imgResultado21;
+  @FXML private ImageView imgResultado22;
+  @FXML private ImageView imgResultado23;
+  @FXML private ImageView imgResultado24;
+  @FXML private ImageView imgResultado25;
+  @FXML private ImageView imgResultado26;
+  
+  @FXML private ImageView imgResultado30; 
+  @FXML private ImageView imgResultado31;
+  @FXML private ImageView imgResultado32;
+  @FXML private ImageView imgResultado33;
+  @FXML private ImageView imgResultado34;
+  @FXML private ImageView imgResultado35;
+  @FXML private ImageView imgResultado36;
+  
+  @FXML private ImageView imgResultado40; 
+  @FXML private ImageView imgResultado41;
+  @FXML private ImageView imgResultado42;
+  @FXML private ImageView imgResultado43;
+  @FXML private ImageView imgResultado44;
+  @FXML private ImageView imgResultado45;
+  @FXML private ImageView imgResultado46;
+
+	// Labels nombre género por fila
+	@FXML	private Label lblGenero0;
+	@FXML	private Label lblGenero1;
+	@FXML	private Label lblGenero2;
+	@FXML	private Label lblGenero3;
+	@FXML	private Label lblGenero4;
+
+  // Contenedores VBox de los resultados 
+	@FXML	private VBox line0;
+	@FXML	private VBox line1;
+	@FXML	private VBox line2;
+	@FXML	private VBox line3;
+	@FXML	private VBox line4;
+	
+  @FXML
+  void goToTitulo(MouseEvent event) {
+  	// Va al detalle del titulo/imagen seleccionado
+  }
+
+	@FXML
+	void initialize() {
+
+	}
+
+}
